ff95960594ef75b5f2b76703e00ef0da25f57f3d
yogesh028-coder <yogeshiitbhub@gmail.com>
2020-09-09 00:43:12 +0530

    updated express sessions

---
+++ conFusionServer/models/promos.js
@@ -0,0 +1,41 @@
+const mongoose =require('mongoose');
+const Schema =mongoose.Schema;
+require('mongoose-currency').loadType(mongoose);
+const currency = mongoose.Types.Currency;
+ 
+const promo_schema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        required: true
+
+    },
+    label: {
+        type: String,
+        default: ' '
+
+    },
+    price: {
+        type: currency,
+        required: true,
+        min:0
+
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    featured: {        
+        type: Boolean,
+        default: false
+    }
+});
+
+
+var Promos =mongoose.model('Promo', promo_schema);
+
+module.exports=Promos;
